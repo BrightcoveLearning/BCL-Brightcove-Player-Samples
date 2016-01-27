7673c497018b936fdca463365f0ac703291ebb31
rcrooks <rcrooks@brightcove.com>
2016-01-27 14:04:57 -0500

    update custom playlist script

--- custom-playlist/custom-playlist.js
+++ custom-playlist/custom-playlist.js
@@ -37,7 +37,8 @@ videojs.plugin('customPlaylist', function (options) {
             videoItem,
             playerWidth = isDefined(options.width) ? options.width : myPlayer.width(),
             playerHeight = (9 / 16) * playerWidth;
-
+            console.log(playerWidth);
+            console.log(playerHeight);
         /**
          * removes highlight from all playlist items
          */
